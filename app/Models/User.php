a74309d68d2e76e81917ee7d257034e919e7b279
carry <sikedak7@163.com>
2021-05-08 12:59:03 +0000

    增加一些解释内容

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,13 +7,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+// Laravel 默认为我们生成了用户模型文件
+
 class User extends Authenticatable
 {
+    // Notifiable:消息通知相关功能引用
+    // HasFactory:模型工厂相关功能的引用
+    // Authenticatable 是授权相关功能的引用。
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
-     *
+     * Laravel 在用户模型中默认为我们添加了 fillable 在过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新：
+     * 防止被SQL注入等方式进行破坏
      * @var array
      */
     protected $fillable = [
@@ -24,7 +30,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * 用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
      * @var array
      */
     protected $hidden = [
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::any('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+
+# 路由器的另外一种配置方式
+Route::get('test', function(){
+    return "this is a test";
+});
