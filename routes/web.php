4c19ba5acec44c4913dc479534bf9a808cdd20ed
carry <sikedak7@163.com>
2021-05-08 05:48:16 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+#use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+Route::get('signup', 'UsersController@create')->name('signup');
