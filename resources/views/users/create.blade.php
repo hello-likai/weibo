1e555061c8de7c565bf9caa9959f5b32197646b9
carry <sikedak7@163.com>
2021-05-09 06:39:28 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,9 @@
       <h5>注册</h5>
     </div>
     <div class="card-body">
+        <!-- 引入通用错误页面 -->
+        @include('shared._errors')
+
       <form method="POST" action="{{ route('users.store') }}">
         {{ csrf_field() }}
 
