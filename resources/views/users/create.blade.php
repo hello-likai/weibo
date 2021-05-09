82d8e3723e867fead9eb78fee12f157f6a081cf9
carry <sikedak7@163.com>
2021-05-09 06:34:50 +0000

    增加CSRF验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,29 +9,31 @@
     </div>
     <div class="card-body">
       <form method="POST" action="{{ route('users.store') }}">
-          <div class="form-group">
-            <label for="name">名称：</label>
-            <!-- Laravel 提供了全局辅助函数 old 来帮助我们在 Blade 模板中显示旧输入数据。这样当我们信息填写错误，
+        {{ csrf_field() }}
+
+        <div class="form-group">
+          <label for="name">名称：</label>
+          <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          <!-- Laravel 提供了全局辅助函数 old 来帮助我们在 Blade 模板中显示旧输入数据。这样当我们信息填写错误，
                 页面进行重定向访问时，输入框将自动填写上最后一次输入过的数据 -->
-            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
-          </div>
+        </div>
 
-          <div class="form-group">
-            <label for="email">邮箱：</label>
-            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
-          </div>
+        <div class="form-group">
+          <label for="email">邮箱：</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
 
-          <div class="form-group">
-            <label for="password">密码：</label>
-            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-          </div>
+        <div class="form-group">
+          <label for="password">密码：</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
 
-          <div class="form-group">
-            <label for="password_confirmation">确认密码：</label>
-            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
-          </div>
+        <div class="form-group">
+          <label for="password_confirmation">确认密码：</label>
+          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+        </div>
 
-          <button type="submit" class="btn btn-primary">注册</button>
+        <button type="submit" class="btn btn-primary">注册</button>
       </form>
     </div>
   </div>
