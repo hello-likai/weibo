521d9ca0645ea284c77952beca2fffb79bddd6b7
carry <sikedak7@163.com>
2021-05-10 03:55:13 +0000

    实现登出功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,7 +9,7 @@ class SessionsController extends Controller
 {
     // 登陆视图
     public function create(){
-        return view('session.create');
+        return view('sessions.create');
     }
 
     // 创建 store 动作来对用户提交的数据进行验证
@@ -28,5 +28,14 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
 
+    }
+
+    // 登出逻辑
+    public function destroy()
+    {
+        // Laravel 默认提供的 Auth::logout() 方法来实现用户的退出功能
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
